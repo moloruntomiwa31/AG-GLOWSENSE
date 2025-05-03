93f78b970f30a973e97dae32f1902b068e333336
moloruntomiwa31 <moloruntomiwa31@gmail.com>
2025-05-03 16:28:19 +0100

    chore: vue devtools

--- vite.config.js
+++ vite.config.js
@@ -2,19 +2,20 @@ import { fileURLToPath, URL } from "node:url";
 
 import { defineConfig } from "vite";
 import vue from "@vitejs/plugin-vue";
+import vueDevTools from "vite-plugin-vue-devtools";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()],
+  plugins: [vue(), vueDevTools()],
   resolve: {
     alias: {
       "@": fileURLToPath(new URL("./src", import.meta.url)),
     },
   },
   server: {
-    host: true, 
+    host: true,
   },
   optimizeDeps: {
-    exclude: ['@google/genai']
-  }
+    exclude: ["@google/genai"],
+  },
 });
