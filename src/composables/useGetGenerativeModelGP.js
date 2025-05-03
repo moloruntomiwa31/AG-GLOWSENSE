ae6757a136137958cf0acefeba0f8ee10ed03236
moloruntomiwa31 <moloruntomiwa31@gmail.com>
2025-05-03 16:17:09 +0100

    chore: genai fix

--- src/composables/useGetGenerativeModelGP.js
+++ src/composables/useGetGenerativeModelGP.js
@@ -1,11 +1,6 @@
 import { useGenAi } from "./useGenAi.js";
 
 export const useGetGenerativeModelGP = async (prompt) => {
-  const newPrompt = `You're an AI Assistant specialised in skincare, beauty and cosmetics related topics. Please provide a very brief, accurate and helpful information for the following question: ${prompt}`;
-  const model = await useGenAi("gemini-1.5-pro");
-  const result = await model.generateContent(newPrompt);
-  const response = result.response;
-  const text = response.text();
-
+  const text = await useGenAi(prompt);
   return text;
 };
